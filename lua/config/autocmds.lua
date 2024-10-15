ac31c212e6722defdf5fbef3047f98b2739acfd3
Дубовской Максим Дмитриевич <dubovskoy@5042.ru>
2024-10-15 14:49:37 +0300

    wip: autocmds for change file encoding

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,21 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+    return vim.api.nvim_create_augroup("max_" .. name, {clear = true})
+end
+
+vim.api.nvim_create_autocmd({"BufReadPost"}, {
+    group    = augroup("koi8r"),
+    pattern  = {"*.lua", "*.c"},
+    callback = function(event)
+        local s = vim.api.nvim_buf_get_lines(event.buf, 0, 1, true)[1]
+        if s == nil then return end
+        local encoding = s:match("encoding +(%w+)")
+        if encoding then
+            vim.cmd(string.format(":e ++enc=%s", encoding))
+        end
+        -- vim.notify(vim.inspect(result))
+    end,
+})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,39 +1,34 @@
 return {
-  {
-    'morhetz/gruvbox',
-    enabled = true,
-    config = function()
-      -- vim.g.gruvbox_bold = 0
-      -- vim.g.gruvbox_contrast_dark = 'soft'
-      -- vim.cmd.colorscheme 'gruvbox'
-    end
-  },
-  {
-    'navarasu/onedark.nvim',
-    enabled = true
-  },
-  {
-    'catppuccin/nvim',
-    enabled = true
-  },
-  {
-    'savq/melange-nvim',
-    enabled = true,
-  },
-  {
-    'romainl/Apprentice',
-    enabled = true,
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    enabled = true,
-  },
-
-  -- Configure LazyVim to load colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "melange",
+    {
+        'morhetz/gruvbox',
+        enabled = true,
     },
-  },
+    {
+        'navarasu/onedark.nvim',
+        enabled = true
+    },
+    {
+        'catppuccin/nvim',
+        enabled = true
+    },
+    {
+        'savq/melange-nvim',
+        enabled = true,
+    },
+    {
+        'romainl/Apprentice',
+        enabled = true,
+    },
+    {
+        'rebelot/kanagawa.nvim',
+        enabled = true,
+    },
+
+    -- Configure LazyVim to load colorscheme
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "melange",
+        },
+    },
 }
