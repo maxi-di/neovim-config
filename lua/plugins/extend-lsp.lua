c928ed1c0cbc70fced8a12059d336eb577c9d0d2
Дубовской Максим Дмитриевич <dubovskoy@5042.ru>
2024-10-15 14:50:34 +0300

    wip: add some lsp's

--- lua/plugins/extend-lsp.lua
+++ lua/plugins/extend-lsp.lua
@@ -10,9 +10,15 @@ return {
             },
         },
     },
-    {
-        import = "lazyvim.plugins.extras.lang.json",
-    },
+    {import = "lazyvim.plugins.extras.lang.json"},
+    {import = "lazyvim.plugins.extras.lang.clangd"},
+    {import = "lazyvim.plugins.extras.lang.cmake"},
+    {import = "lazyvim.plugins.extras.lang.docker"},
+    {import = "lazyvim.plugins.extras.lang.git"},
+    {import = "lazyvim.plugins.extras.lang.go"},
+    {import = "lazyvim.plugins.extras.lang.markdown"},
+    {import = "lazyvim.plugins.extras.lang.toml"},
+    {import = "lazyvim.plugins.extras.lang.yaml"},
     {
         "stevearc/conform.nvim",
         opts = {
