3a862ea2ac037f7615cda30ee13329b2745c162c
Дубовской Максим Дмитриевич <dubovskoy@5042.ru>
2024-10-17 16:14:25 +0300

    wip: keymaps 'Open Trouble for telescope' -> <leader>x, <leader>ct

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>ct", "<cmd>Trouble telescope<cr>", {desc = "Trouble from telescope"})
+vim.keymap.set("n", "<leader>x,", "<cmd>Trouble telescope<cr>", {desc = "Trouble from telescope"})
