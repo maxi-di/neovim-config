ae342c9fb009591fd639fa4ab779f2448e4d0a15
Дубовской Максим Дмитриевич <dubovskoy@5042.ru>
2024-10-23 08:54:04 +0300

    wip: session autoload

--- lua/plugins/neovim-session-manager.lua
+++ lua/plugins/neovim-session-manager.lua
@@ -1,13 +1,16 @@
 return {
     {
         "Shatur/neovim-session-manager",
+        lazy = false,
         dependencies = {
             "nvim-lua/plenary.nvim", -- required
         },
         cmd = "SessionManager",
-        opts = {
-            sessions_dir = require('plenary.path'):new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
-        },
+        opts = function(_, opts)
+            local config       = require('session_manager.config')
+            opts.sessions_dir  = require('plenary.path'):new(vim.fn.stdpath('data'), 'sessions') -- The directory where the session files will be saved.
+            opts.autoload_mode = {config.AutoloadMode.CurrentDir, config.AutoloadMode.LastSession}
+        end,
         keys = {
             {"<leader>P", "<cmd>SessionManager load_session<cr>", desc = "Open session"},
             {"<A-l>",     "<cmd>SessionManager load_session<cr>", desc = "Open session"},
