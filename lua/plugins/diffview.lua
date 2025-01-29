deb47440d3a503c8b50f8fc20df214697ba1bb84
Дубовской Максим Дмитриевич <dubovskoy@5042.ru>
2025-01-29 15:47:02 +0300

    wip: close diffview on 'q'

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,17 +2,28 @@ return {
     {
         "sindrets/diffview.nvim",
         dependencies = {'nvim-tree/nvim-web-devicons'},
-        opts = {
-            view = {
+        opts = function(_, opts)
+            opts.view = {
                 merge_tool = {
                     layout = "diff3_mixed",
                 }
             }
-        },
+
+            local function augroup(name)
+                return vim.api.nvim_create_augroup("max_" .. name, {clear = true})
+            end
+
+            vim.api.nvim_create_autocmd('FileType', {
+                group = augroup("custom_diffview_keymaps"),
+                pattern = "DiffviewFiles",
+                callback = function(args)
+                    vim.keymap.set('n', 'q', '<cmd>DiffviewClose<cr>',
+                        {buffer = args.buf, desc = 'Close diffview'})
+                end
+            })
+        end,
         keys = {
-            {"<leader>go", "<cmd>DiffviewOpen<cr>", desc = "DiffviewOpen"},
-            --     {"<leader>dc", "<cmd>DiffviewClose<cr>", desc = "DiffviewClose"},
-            --     {"<A-g>",      "<cmd>DiffviewOpen<cr>",  desc = "DiffviewOpen"},
+            {"<leader>go", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview"},
         }
     }
 }
