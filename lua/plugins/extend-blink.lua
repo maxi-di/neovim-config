359f6f621b45c758a5041815721a96c56e7ab189
Дубовской Максим Дмитриевич <dubovskoy@5042.ru>
2025-01-29 11:05:25 +0300

    fix: don't blink completion in Telescope

--- lua/plugins/extend-blink.lua
+++ lua/plugins/extend-blink.lua
@@ -16,7 +16,10 @@ return {
             },
             enabled = function()
                 -- Отключаем автодополнение для определенного типа файлов
-                return not vim.tbl_contains({"typr"}, vim.bo.filetype)
+                return not vim.tbl_contains({
+                    "typr",
+                    "TelescopePrompt",
+                }, vim.bo.filetype)
             end,
         },
     },
