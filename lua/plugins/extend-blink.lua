ccc7d196c773f0284aed07599df5334b1551bff4
Дубовской Максим Дмитриевич <dubovskoy@5042.ru>
2025-04-01 16:13:58 +0300

    wip: add colorful-menu for blink.nvim

--- lua/plugins/extend-blink.lua
+++ lua/plugins/extend-blink.lua
@@ -1,5 +1,9 @@
 return {
     {
+        "xzbdmw/colorful-menu.nvim",
+        config = true,
+    },
+    {
         "saghen/blink.cmp",
         opts = {
             keymap = {
@@ -7,6 +11,24 @@ return {
                 ["<C-g>"] = {'show', 'show_documentation', 'hide_documentation'},
             },
             completion = {
+                menu = {
+                    border = 'rounded',
+                    draw = {
+                        -- We don't need label_description now because label and label_description are already
+                        -- combined together in label by colorful-menu.nvim.
+                        columns = {{"kind_icon"}, {"label", gap = 1}},
+                        components = {
+                            label = {
+                                text = function(ctx)
+                                    return require("colorful-menu").blink_components_text(ctx)
+                                end,
+                                highlight = function(ctx)
+                                    return require("colorful-menu").blink_components_highlight(ctx)
+                                end,
+                            },
+                        },
+                    },
+                },
                 list = {
                     selection = {
                         preselect = true,
