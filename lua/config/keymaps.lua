ece20ee2bd47236a07a786caabf0193022ab5984
Дубовской Максим Дмитриевич <dubovskoy@5042.ru>
2025-10-30 10:24:05 +0300

    feat: harpoon imitation ("<leader>a", "<leader>[1..4]")

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,3 +29,26 @@ vim.keymap.set("n", "<C-p>", LazyVim.pick("files", {root = false}), {desc = "Fin
 vim.keymap.set("n", "<leader>s/", LazyVim.pick("live_grep", {search_dirs = {"%:p"}}), {desc = "Find Files (cwd)"})
 
 vim.keymap.set("n", "<A-o>", "<cmd>ClangdSwitchSourceHeader<CR>", {desc = "Clang switch header/source file"})
+
+-- Harpoon imitation
+local function harpoon3()
+    vim.keymap.set("n", "<leader>a", function()
+        vim.cmd("silent! argadd %")
+        vim.cmd("silent! argdedupe")
+    end, {desc = "add to Harpoon"})
+
+    vim.keymap.set("n", "<leader>1", function()
+        vim.cmd("silent! 1argument")
+    end, {desc = "Harpoon 1 argument"})
+    vim.keymap.set("n", "<leader>2", function()
+        vim.cmd("silent! 2argument")
+    end, {desc = "Harpoon 2 argument"})
+    vim.keymap.set("n", "<leader>3", function()
+        vim.cmd("silent! 3argument")
+    end, {desc = "Harpoon 3 argument"})
+    vim.keymap.set("n", "<leader>4", function()
+        vim.cmd("silent! 4argument")
+    end, {desc = "Harpoon 4 argument"})
+end
+
+harpoon3()
